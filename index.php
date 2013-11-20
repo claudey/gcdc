d7ed4c5d84e722cfd6a173e78c4900ee426a282d
Claude Ayitey <clauditey@gmail.com>
2013-11-20 20:30:22 +0000

    corrected the labels on the navbar to make the consistent.

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
         		
         		<ul class="nav navbar-nav navbar-right">
         			<li class="dropdown">
-        				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+        				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Officials <b class="caret"></b></a>
         				<ul class="dropdown-menu">
         					<li><a href="/follow#mp">MPs</a></li>
         					<li><a href="/follow#dce">DCEs</a></li>
