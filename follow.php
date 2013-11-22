469c099e2f79a21f59969cd6be257eacbd23f0b0
Albert Fiati-Kumasenu <awkfiati@gmail.com>
2013-11-22 06:38:29 +0000

    added header for follow page

--- follow.php
+++ follow.php
@@ -12,39 +12,9 @@
     </head>
     <body>
         <div class="container main">
-	        <nav class="navbar navbar-default" role="navigation">
-	        	<!-- Brand and toggle get grouped for better mobile display -->
-	        	<div class="navbar-header">
-	        		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-	        			<span class="sr-only">Toggle navigation</span>
-	        			<span class="icon-bar"></span>
-	        			<span class="icon-bar"></span>
-	        			<span class="icon-bar"></span>
-	        		</button>
-	        		<a class="navbar-brand" href="#">Leader Watch</a>
-	        	</div>
-
-	        	<!-- Collect the nav links, forms, and other content for toggling -->
-	        	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	        		
-	        		<ul class="nav navbar-nav navbar-right">
-
-	        			<li class="dropdown">
-	        				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Officials <b class="caret"></b></a>
-	        				<ul class="dropdown-menu">
-	        					<li><a href="/follow#mp">MPs</a></li>
-	        					<li><a href="/follow#dce">DCEs</a></li>
-	        					<li><a href="/follow#min">Ministers</a></li>
-	        					<li class="divider"></li>
-	        					<li><a href="/follow#prez">The President</a></li>
-	        				</ul>
-	        			</li>
-						<li><a href="#">Create Issue</a></li>
-						<li><a href="#">Sign up / Login</a></li>
-
-	        		</ul>
-	        	</div><!-- /.navbar-collapse -->
-	        </nav>
+	        <?php
+                @include("header.php");
+            ?>
 
         	<div class="row">
 
@@ -58,7 +28,6 @@
         		</nav>
 
         		<div class="feed col-md-8 col-md-offset-2">
-        			<p class="welcome">Welcome to GCDC. Click on any of the tabs to follow .</p>
         			<div class="fd feed-mp">
         				
                         <!-- Ashanti Region -->
